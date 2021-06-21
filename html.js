cb642ee3eb1834a70ac28da380b1cf5df3df9a84
arj55 <58042319+arj55@users.noreply.github.com>
2021-06-21 10:25:55 +0530

    Create html.js

---
+++ html.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+
+</head>
+
+<body>
+
+
+    <center>
+        <h2 style="font-size: 50px;font-family: 'Courier New', Courier, monospace;">Weather Forecast</h2>
+        <div>
+            <form action="/" method="POST">
+                <label for="cityInput"><b>CITY : </b></label>
+                <input id="cityInput" type="text" name="CityName">
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </center>
+</body>
+
+</html> 
